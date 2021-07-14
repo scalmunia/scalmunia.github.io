ba1490ddf28ba120e0ad5929c03a7a9a16b589c1
scalmunia <scalmunia@gmail.com>
2021-07-14 12:18:50 +0200

    feat(vacaciones): second counter

--- vacaciones/static/js/main.js
+++ vacaciones/static/js/main.js
@@ -7,7 +7,7 @@ function $(selector) {
 function App() {
   // Declaraciones de la App
 
-  const calcCounter = () => {
+  const calcCounterOnboarding = () => {
     const now = moment();
     const onboarding = moment("21/07/2021 06:40:00", "DD/MM/YYYY HH:mm:ss");
     const diff = onboarding.diff(now);
@@ -16,9 +16,18 @@ function App() {
     return counters;
   };
 
-  const renderCounter = () => {
+  const calcCounterTestDate = () => {
+    const now = moment();    
+    const testDate = moment("19/07/2021 06:40:00", "DD/MM/YYYY HH:mm:ss");
+    const diff = testDate.diff(now);
+    const counters = moment.utc(diff).format("DD_HH_mm_ss").split("_");
+
+    return counters;
+  };
+  
+  const renderCounters = () => {
     const labels = ["dias", "horas", "minutos", "segundos"];
-    const countdown = calcCounter()
+    const countdownOnboarding = calcCounterOnboarding()
       .map(
         (counter, index) => /*html*/ `
         <div class="counter">
@@ -29,13 +38,30 @@ function App() {
       )
       .join("");
 
-    $("#main-countdown").innerHTML = countdown;
+    $("#main-countdown").innerHTML = countdownOnboarding;
+
+    const countdownTestDate = calcCounterTestDate()
+      .map(
+        (counter, index) => /*html*/ `
+        <div class="counter">
+          <div class="amount">${counter}</div>
+          <div class="label">${labels[index]}</div>
+        </div>
+      `
+      )
+      .join("");
+
+    $("#second-countdown").innerHTML = countdownTestDate;
   };
 
+   
+
   // Ejecuciones de la App
-  renderCounter();
-  setInterval(renderCounter, 1000);
-}
+  renderCounters();
+  setInterval(renderCounters, 1000);
+};
 
 // Ejecuciones
 document.addEventListener("DOMContentLoaded", () => App());
+
+
